86a12092af28b5c5dfcaa26417871ee35518b6f5
hermasyp <hermas.yuda@gmail.com>
2019-03-23 09:45:40 +0700

    initial project

---
+++ app/src/main/java/com/catnip/bikecident/data/remote/NetworkClient.java
@@ -0,0 +1,19 @@
+package com.catnip.bikecident.data.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Written with love by Muhammad Hermas Yuda Pamungkas
+ * Github : https://github.com/hermasyp
+ **/
+public class NetworkClient {
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.thesportsdb.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/catnip/bikecident/data/remote/Routes.java
@@ -0,0 +1,9 @@
+package com.catnip.bikecident.data.remote;
+
+/**
+ * Written with love by Muhammad Hermas Yuda Pamungkas
+ * Github : https://github.com/hermasyp
+ **/
+public interface Routes {
+
+}
